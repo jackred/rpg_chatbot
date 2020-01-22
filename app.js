31fb974b6a7702d8223dd1d4221f46f54f465a6c
Jackred <jackred@tuta.io>
2020-01-22 11:38:18 +0000

    ADD mongo storage config
    store config in mongo now

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const Discord = require('discord.js');
 const request = require('request-promise-native');
 const uuidv1 = require('uuid/v1');
+const mongoose = require('mongoose');
 const config = require('./config.json');
+const { config_schema } = require('./src/model/config');
+
+mongoose.connect(config.mongo, {useNewUrlParser: true});
+// mongoose.connect("mongodb://172.18.0.2:27017/alana", {useNewUrlParser: true});
+const config_model = mongoose.model('configs', config_schema);
 
 
 const client = new Discord.Client();
@@ -21,25 +27,27 @@ function send_embed(channel, text, author)  {
 }
 
 
-
 function get_template() {
   return require('./config_alana.json');
 }
+
 
 function create_session_id() {
   return `${config.project}.${uuidv1()}`;
 
 }
 
-function build_request(question) {
-  let data = get_template();
+
+function build_request(question, data) {
   data.question = question;
   data.session_id = create_session_id();
+  data.user_id = config.user_id;
   return data;
 }
 
-function build_options(question, uri=config.alana) {
-  const data = build_request(question);
+
+function build_options(question, data_bot, uri=config.alana) {
+  const data = build_request(question, data_bot);
   return {
     method: 'POST',
     uri: uri,
@@ -48,8 +56,9 @@ function build_options(question, uri=config.alana) {
   };
 }
 
-function request_alana(question) {
-  const options = build_options(question);
+
+function request_alana(question, request_config) {
+  const options = build_options(question, request_config.data);
   return request.post(options)
     .then(res => {
       console.log(`ANSWER: ${res.result}`);
@@ -59,17 +68,25 @@ function request_alana(question) {
 }
 
 
-client.on('message', async (message) => {
-  if (message.content.startsWith('!')) {
-    res = await request_alana(message.content.substr(1));
+function db_find_config(prefix) {
+  console.log(prefix);
+  return config_model.findOne({prefix}).exec();
+}
+
+
+async function answer(message) {
+  const request_config = await db_find_config(message.content[0]);
+  console.log( `INFO: prefix found: ${request_config !== null}, ${request_config}`);
+  if (request_config !== null) { // else not a prefix, regular message
+    res = await request_alana(message.content.substr(1), request_config);
     send_embed(message.channel, res.result, res.bot_name);
   }
-});
+}
+
+
+client.on('message', answer);
 
 
 client.login(config.token)
   .then(() => console.log("We're in!"))
   .catch((err) => console.log(err));
-
-
-
