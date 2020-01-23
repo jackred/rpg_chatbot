34b90d694beef39c15de3c275a371bbddb21ef33
Jackred <jackred@tuta.io>
2020-01-23 00:31:44 +0000

    FIX again parser
    error in previous fixing

--- src/AlanaParser.js
+++ src/AlanaParser.js
@@ -6,9 +6,12 @@ const AlanaUtility = require('./AlanaUtility');
 function prefixParser(text) {
   let res = -1;
   for (let prefix in config.prefix) { // yeah iterating over all keys when I would have do a while and stop when needed...but I'm lazy this time
-    console.log(prefix);
-    if (text.startsWith(prefix)){
-      let rest = text.replace(text, '');
+    console.log(prefix, config.prefix[prefix]);
+    console.log('text', text);
+    console.log('start? ', text.startsWith(config.prefix[prefix]));
+    if (text.startsWith(config.prefix[prefix])){
+      let rest = text.replace(config.prefix[prefix], '');
+      console.log('->>', rest);
       res = {'first': config.prefix[prefix], 'rest': rest.trim()};
     }
   }
